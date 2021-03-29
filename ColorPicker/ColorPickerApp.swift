90bbfc36a9edb15c6ce9b74176af2357e1000a95
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-03-30 00:32:46 +0300

    Initial Commit

---
+++ ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by ALEKSEY SUSLOV on 30.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
